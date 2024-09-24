e8a2eb490f80f890be7532e128253e3358f2ebac
SIA - Chiboub <sia.o.chiboub@laval.ca>
2024-09-24 10:25:51 -0400

    attempt to hide footer 3

--- Clavardage.py
+++ Clavardage.py
@@ -263,6 +263,7 @@ def send_request_to_custom_api(user_input):
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
